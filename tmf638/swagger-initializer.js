2dfbdb3bbb245c1f0a04a959f837697822936677
timzhang <tim.1.zhang@nokia.com>
2023-06-06 16:10:21 -0400

    Add tmfapi

--- tmf638/swagger-initializer.js
+++ tmf638/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "TMF638-ServiceInventory-v4.0.0.swagger.json",
+    url: "https://raw.githubusercontent.com/zhangxd926/tmforum-open-apis/main/tmf638/TMF638-ServiceInventory-v4.0.0.swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
