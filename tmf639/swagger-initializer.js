539ef27db2cdd8a36e0e305bd803bfc3ecd47aca
timzhang <tim.1.zhang@nokia.com>
2023-06-06 16:12:15 -0400

    Add tmfapi

--- tmf639/swagger-initializer.js
+++ tmf639/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/kubernetes/kubernetes/master/api/openapi-spec/swagger.json",
+    url: "https://raw.githubusercontent.com/zhangxd926/tmforum-open-apis/main/tmf639/TMF639-ResourceInventory-v4.0.0.swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
